96a2fa29725cad871dc76f5f736b6d27e015a388
Ted Zhou <zxxufo008@gmail.com>
2023-08-19 08:57:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def main():
             time.sleep(random.randint(1,5))
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
-    send_email(favorites)
+    #send_email(favorites)
     logger.info("所有用户签到结束")
 
 
